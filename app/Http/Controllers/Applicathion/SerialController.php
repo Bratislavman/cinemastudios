4ba23dc281374f30600b63fc5394b9f8a94336e0
kir <kiro241@gmail.com>
2020-05-19 19:01:45 +0300

    перекинул модель сериала

---
+++ app/Http/Controllers/Applicathion/SerialController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Serial;
+
+class SerialController extends Controller
+{
+    public function index()
+    {
+        return '';
+    }
+
+    public function create()
+    {
+        return '';
+    }
+
+    public function update(Serial $serial)
+    {
+        return '';
+    }
+}
